d12b64e36224cb646f1b5d300d4eea44d30d8623
Paul C <paulcollinsaf@gmail.com>
2019-10-27 09:24:53 +1000

    $(git branch-name): bugfix

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
+var instructionsRouter = require('./routes/instructions');
 var usersRouter = require('./routes/users');
 
 var app = express();
@@ -15,6 +16,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
+app.use('/instructions', instructionsRouter);
 app.use('/users', usersRouter);
 
 module.exports = app;
