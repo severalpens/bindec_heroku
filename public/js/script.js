f9a569c4a27769c84235a8636224725c8fdcc381
severalpens <paulcollinsaf@gmail.com>
2020-04-28 11:10:57 +1000

    master

--- public/js/script.js
+++ public/js/script.js
@@ -1,112 +1,109 @@
-$(document).ready(function() {
-  const timeoutSeconds        =   20;
-  const progressBar         =     document.getElementById("progressBar");
-  const responseInput         =   $("#responseInput");
-  const submittedBinary       =   $("#submittedBinary");
-  const submittedDecimal      =   $("#submittedDecimal");
-  const submittedResponse     =   $("#submittedResponse");
-  const submittedResult       =   $("#submittedResult");
-  const q = {decimal: 0, bits: ""};
-  const score = {correct: 0, attempts: 0};
+$(document).ready(function () {
+  const timeoutSeconds = 20;
+  const progressBar = document.getElementById("progressBar");
+  const responseInput = $("#responseInput");
+  const submittedBinary = $("#submittedBinary");
+  const submittedDecimal = $("#submittedDecimal");
+  const submittedResponse = $("#submittedResponse");
+  const submittedResult = $("#submittedResult");
+  const q = {
+    decimal: 0,
+    bits: ""
+  };
+  const score = {
+    correct: 0,
+    attempts: 0
+  };
   var paused = false;
-var restartCounter = false
+  var restartCounter = false;
 
-
-  function init(){
+  function init() {
     $("#score").text(`Score: 0/0`);
     score.correct = 0;
     score.attempts = 0;
     progressBar.value = timeoutSeconds;
     progressBar.max = timeoutSeconds;
   }
-  
-  function newQuestion(){
-   var difficultyLevel     = document.getElementById("difficultyLevel").options.selectedIndex;
-   switch (difficultyLevel) {
-    case 0:
-      q.decimal = Math.floor((Math.random() * 15) + 1);        
-      break;
-  
-      case 1:
-       
-        q.decimal = 16 * Math.floor((Math.random() * 15) + 1);        
+
+  function newQuestion() {
+    var difficultyLevel = document.getElementById("difficultyLevel").options.selectedIndex;
+    switch (difficultyLevel) {
+      case 0:
+        q.decimal = Math.floor((Math.random() * 15) + 1);
         break;
-      
-        case 2:
-       
-          q.decimal = Math.floor((Math.random() * 255) + 1);        
-          break;
-          
-        default:
-       break;
-   }
+      case 1:
+        q.decimal = 16 * Math.floor((Math.random() * 15) + 1);
+        break;
+      case 2:
+        q.decimal = Math.floor((Math.random() * 255) + 1);
+        break;
+      default:
+        break;
+    }
 
-if(score.attempts % 7 == 0){
-    q.decimal = 8//168
-}
-if(score.attempts % 9 == 0){
-  q.decimal = 7//192
-}
+    if (score.attempts % 7 == 0) {
+      q.decimal = 8 //168
+    }
 
-   var rawBits = "00000000" + q.decimal.toString(2);
+    if (score.attempts % 9 == 0) {
+      q.decimal = 7 //192
+    }
+
+    var rawBits = "00000000" + q.decimal.toString(2);
     q.bits = rawBits.slice(-8);
     $("#bits").text(q.bits);
   };
-  
-  
+
   init();
   newQuestion();
 
-  
 
-  
-  
+  $("#difficultyLevel").change(function () {
+    location.reload();
+  });
 
-  function processAnswer(timedOut){
+
+
+  function processAnswer(timedOut) {
     submittedBinary.text(q.bits);
     submittedDecimal.text(q.decimal);
-    if(timedOut){
+    if (timedOut) {
       submittedResponse.text("Timeout");
       submittedResult.text("Incorrect");
       return 0;
-    }
-    else{
-      let response =  parseInt(responseInput.val());
+    } else {
+      let response = parseInt(responseInput.val());
       let result = response === q.decimal;
       submittedResponse.text(response);
       submittedResult.text(result ? "Correct" : "Incorrect");
       return result;
     }
   }
-  
-  
-  function updateScore(result){
-    console.log("update Score: " + result)
-     score.attempts ++;
-      score.correct +=  result;
-      $("#score").text(`Score: ${score.correct}/${score.attempts}`);
-      if(result){
-        console.log("success")
-        $("#responseInput").addClass("success");
-        $("#responseInput").removeClass("failure");
-      }else{
-        console.log("fail")
-        $("#responseInput").addClass("failure");
-        $("#responseInput").removeClass("success");
-      }
 
+  function updateScore(result) {
+    console.log("update Score: " + result)
+    score.attempts++;
+    score.correct += result;
+    $("#score").text(`Score: ${score.correct}/${score.attempts}`);
+    if (result) {
+      console.log("success")
+      $("#responseInput").addClass("success");
+      $("#responseInput").removeClass("failure");
+    } else {
+      console.log("fail")
+      $("#responseInput").addClass("failure");
+      $("#responseInput").removeClass("success");
+    }
   }
 
 
   function startCountdown(secondsRemaining) {
-
     setInterval(() => {
       $("#progressBar").val(secondsRemaining);
       secondsRemaining = paused ? secondsRemaining : secondsRemaining - 1;
-
-      if(restartCounter){
+      if (restartCounter) {
         secondsRemaining = timeoutSeconds;
-        
+
         restartCounter = false;
       }
       if (secondsRemaining < 0) {
@@ -115,37 +112,38 @@ if(score.attempts % 9 == 0){
         updateScore(0);
         newQuestion();
         restartCounter = true
-      
+
       }
     }, 1000);
   }
 
-  
-  $("#pause").click(function(e){
+
+  $("#pause").click(function (e) {
     e.preventDefault();
     paused = !paused;
     $(this).text(paused ? "Resume" : "Pause");
-})
+  })
 
 
   function responseSubmitHandler(event) {
-    event.preventDefault();
+    event.preventDefault();    
+    console.log(event);
     updateScore(processAnswer(false));
     responseInput.val("");
     paused = false;
     restartCounter = true;
     $("#pause").text("Pause");
     newQuestion();
-
   }
 
-  $("#responseSubmit").click(responseSubmitHandler);
-  $("#form").su
+  // $("#responseSubmit").click(responseSubmitHandler);
+  //$("#responseSubmit").blur(responseSubmitHandler);
+  // $("#responseSubmit").submit(responseSubmitHandler);
+  $("#form").submit(responseSubmitHandler);
 
   startCountdown(timeoutSeconds);
 
 
 
- 
-});
 
+});
